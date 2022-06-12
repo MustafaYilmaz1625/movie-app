744ea4db066a857ea159080db0c52a5114b18974
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-12 19:10:07 +0300

    populartyCard

---
+++ src/utils/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0026ca",
+      //   main: "#14213d",
+    },
+    secondary: {
+      main: "#1DE9B6",
+    },
+    common: {
+      drawer: "#A22032",
+    },
+  },
+});
+
+export default theme;
